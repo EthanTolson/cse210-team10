a9aecb0b615a200fc330796593717b3472559810
JWilderman <kaodremora@outlook.com>
2021-09-30 15:39:46 -0400

    fixing a mistake of working on main branch

--- week3code/__main__.py
+++ week3code/__main__.py
@@ -1,8 +1,6 @@
 from tolson import giveGreetingEthan
 from miskin import miskinGreeting
-from wilderman import giveGreetingWilderman
 
 print("Welcome to our collabrative program.")
 giveGreetingEthan()
 miskinGreeting()
-giveGreetingWilderman()
--- week3code/wilderman.py
+++
@@ -1,2 +0,0 @@
-def giveGreetingWilderman():
-    print("Hi from Jace Wilderman.")
