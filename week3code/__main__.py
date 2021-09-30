15488f8c950c080b33a4df67f30f1dd047487d97
JWilderman <kaodremora@outlook.com>
2021-09-30 15:37:09 -0400

    Finishing part 2

--- week3code/__main__.py
+++ week3code/__main__.py
@@ -1,6 +1,8 @@
 from tolson import giveGreetingEthan
 from miskin import miskinGreeting
+from wilderman import giveGreetingWilderman
 
 print("Welcome to our collabrative program.")
 giveGreetingEthan()
 miskinGreeting()
+giveGreetingWilderman()
