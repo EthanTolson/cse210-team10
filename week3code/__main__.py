0dd4baba168bbc35a61239fd01082805efc28638
austin eldredge <eld20009@byui.edu>
2021-09-30 13:40:15 -0600

    altered main to include my function

--- week3code/__main__.py
+++ week3code/__main__.py
@@ -1,5 +1,6 @@
 from tolson import giveGreetingEthan
 from miskin import miskinGreeting
-
+from eldredge import intoduce_eldredge
 print("Welcome to our collabrative program.")
 giveGreetingEthan()
+intoduce_eldredge()
